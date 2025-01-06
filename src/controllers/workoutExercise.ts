5126a9da398e04c452680771b0d0085a6d6735ea
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2025-01-06 10:23:14 -0300

    feat: creating a new route to see the workout exercise for specific user

--- src/controllers/workoutExercise.ts
+++ src/controllers/workoutExercise.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import WorkoutExercise from '../models/workoutExercise';
 import Exercise from '../models/exercise';
 import Workout from '../models/workouts';
+import { Authenticate } from '../middleware/auth';
 
 class WorkoutExerciseController {
   public static async createWorkoutExercise(req: Request, res: Response): Promise<Response> {
@@ -21,6 +22,40 @@ class WorkoutExerciseController {
       const newWorkoutExercise = await WorkoutExercise.create(req.body);
 
       return res.status(200).json({ success: true, newWorkoutExercise: newWorkoutExercise })
+
+    }catch(err){
+      console.error(err);
+      return res.status(500).json({error: err});
+    }
+  }
+
+  public static async getWorkout(req: Authenticate, res: Response): Promise<Response> {
+    try{
+      const workoutId = Number(req.query.id);
+
+      /**
+        select e.id as exercise_id,
+          e.name as exercise_name,
+          we.sets,
+          we.repetitions,
+          w.name
+        from workouts w
+        inner join workout_exercise we on w.id = we."workoutId"
+        inner join exercises e on we."exerciseId" = e.id
+        where w."userId" = 3 and w.id = 1;
+       */
+      const workout = await Workout.findAll({
+        where: { id: workoutId, userId: req.user ? req.user.userId : undefined },
+        include: [{
+          model: WorkoutExercise,
+          include: [{
+            model: Exercise,
+            attributes: ['id', 'name']
+          }]
+        }]
+      });
+
+      return res.status(200).json(workout);
 
     }catch(err){
       console.error(err);
--- src/routes/workoutExercise.ts
+++ src/routes/workoutExercise.ts
@@ -6,6 +6,10 @@ const router = Router();
 
 router.post('/creating', authenticateToken, async(req, res) => {
   await WorkoutExerciseController.createWorkoutExercise(req, res);
-})
+});
+
+router.get('/training?:id', authenticateToken, async(req, res) => {
+  await WorkoutExerciseController.getWorkout(req, res);
+});
 
 export default router;
